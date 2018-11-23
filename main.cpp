4cbacaf513e93d613bc864f4f5d43560f8e81dc6
Frank Hunleth <fhunleth@troodon-software.com>
2018-11-22 22:45:23 -0500

    Add app info in case we use settings

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setApplicationName("Silouette");
+    QApplication::setApplicationVersion("0.1.0");
+    QApplication::setOrganizationName("Hunleth");
+    QApplication::setOrganizationDomain("hunleth.com");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
